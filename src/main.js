f4da2a56a2cf7df7df70c0e6c4966311de685f3d
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-02-05 00:27:54 +0100

    llllllibrary

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import closeIcon from './img/bi_x-octagon.png';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const form = document.querySelector('.form');
